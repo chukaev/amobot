26b7b9c3ede96fc465dca36fcf5ec044fab96088
valeriy <v.gerasimov@innopolis.ru>
2018-01-27 15:18:29 +0300

    debug

--- bot/controllers/amo_integrator.py
+++ bot/controllers/amo_integrator.py
@@ -1,6 +1,9 @@
 from bot import bot
 
+
 def send_to_amo(user, message):
     print(message.__dict__)
     print(message.voice.__dict__)
     print(bot.get_file(message.voice.file_id).__dict__)
+    file = bot.download_file(bot.get_file(message.voice.file_id))
+    print(file.__dict__)
