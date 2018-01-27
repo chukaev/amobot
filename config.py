0ed2fdc36d2d64a54c444f84d16426194d24930a
valeriy <v.gerasimov@innopolis.ru>
2018-01-27 16:30:02 +0300

    Revert "123"
    This reverts commit 4d9713dd007b25063010219c5535047f31e2f4cb, reversing
    changes made to 849506b67c96e52390bb847db4f0b3c61c84e5f9.

---
+++ config.py
@@ -0,0 +1,12 @@
+# This file should not be presented in any public resources. Especially in Version Control system, like github.
+
+token = 'e'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "login in google"
+EMAIL_HOST_PASSWORD = "password in google"
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL=True
+
+main_domain = 'http://46.101.26.12'
