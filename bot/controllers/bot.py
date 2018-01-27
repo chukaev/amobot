8d522c53fad5a197d44c23548a9a9449fe8b95be
valeriy <v.gerasimov@innopolis.ru>
2018-01-27 14:45:44 +0300

    debug

--- bot/controllers/bot.py
+++ bot/controllers/bot.py
@@ -1,10 +1,4 @@
 from telebot import types
-from tgBot import bot
-from tgBot.utils import to_main_page, to_invest_page, to_ref_page, to_pay_page, change_payeer
-from tgBot.models import *
-from tgBot.check import run as run_payment_check
-from messages import before_pay_message, ref_message, invest_message, success_registration
-from config import payeer_account
 
 
 def existed_user_action(user, message):
