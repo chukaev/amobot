4864f7c6091727556caa658d7ec3e6608f7d7f8e
valeriy <v.gerasimov@innopolis.ru>
2018-02-02 23:10:20 +0300

    debug

--- bot/controllers/bot.py
+++ bot/controllers/bot.py
@@ -9,7 +9,7 @@ def existed_user_action(user, message):
     # if user.state == 1:
     #     main_menu_action(user, message) # Function to implement
     # user.save()
-    if message.video:
+    if message.video or message.video_note or message.voice:
         markup = types.InlineKeyboardMarkup()
         pay = types.InlineKeyboardButton(text=pay_button_text, url=main_domain+reverse('payment')+'?id=%d&amount=1' % user.id)
         markup.add(pay)
