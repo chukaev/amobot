dcf4db1749326642a1518e725a538033fd402c4a
valeriy <v.gerasimov@innopolis.ru>
2018-02-02 18:21:23 +0300

    debug

--- bot/views.py
+++ bot/views.py
@@ -41,13 +41,11 @@ def amo_chat_webhook(request):
 
 
 def amo_webhook(request):
-    print(request.GET)
     print(request.POST)
-    print(request.body.decode())
-    if request.method == 'POST':
-        data = json.loads(request.body.decode())
-        proceed_update(data)
-        return HttpResponse(content="Ok", status=200)
+    # if request.method == 'POST':
+        # data = json.loads(request.body.decode())
+        # proceed_update(data)
+    return HttpResponse(content="Ok", status=200)
 
 
 def payment(request):
