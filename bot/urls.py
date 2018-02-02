274c5961396e41832cb4ee9f48d4aea4d8621c2e
valeriy <v.gerasimov@innopolis.ru>
2018-02-02 15:48:16 +0300

    debug amo integrator

--- bot/urls.py
+++ bot/urls.py
@@ -6,7 +6,7 @@ from bot.views import *
 urlpatterns = [
     url(r'^' + token + '$', webhook, name='webhook'),
 
-    url(r'file/(?P<file_id>[*]+)', get_file, name='get_file'),
+    url(r'file/(?P<file_id>.+)', get_file, name='get_file'),
     url(r'amochat/' + 'test', amo_webhook, name='amo_webhook'),
     url(r'amochat/' + scope_id, amo_webhook, name='amo_webhook')
 
