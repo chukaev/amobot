3f2628759ebe62a8c1067ddc155952babe86df9d
valeriy <v.gerasimov@innopolis.ru>
2018-02-02 15:26:40 +0300

    debug amo integrator

--- bot/controllers/amo_integrator.py
+++ bot/controllers/amo_integrator.py
@@ -46,16 +46,16 @@ def send_content(message, body):
             'timestamp': int(time.time()),
             'msgid': str(uuid.uuid4()),
             'conversation_id': 'c' + str(uuid.uuid4()),
-            # 'sender': {
-            #     'id': message.from_user.id,
-            #     'avatar': telegram_file_link % (token, bot.get_file(photos[0][2].file_id).file_path),
-            #     'name': message.from_user.first_name,
-            #     'profile_link': '@valt25',
-            #     'profile': {
-            #         'phone': 89063235383,
-            #         'email': 'v.gerasimov@innopolis.ru'
-            #     }
-            # },
+            'sender': {
+                'id': message.from_user.id,
+                'avatar': telegram_file_link % (token, bot.get_file(photos[0][2].file_id).file_path),
+                'name': message.from_user.first_name,
+                'profile_link': '@valt25',
+                'profile': {
+                    'phone': 89063235383,
+                    'email': 'v.gerasimov@innopolis.ru'
+                }
+            },
             'message': {
                 'type': 'text',
                 'text': body,
