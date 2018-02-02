e328dc7c388821290b005ec785e11b8e7d066baf
valeriy <v.gerasimov@innopolis.ru>
2018-02-02 21:58:38 +0300

    development of getting values from amocrm

--- bot/models.py
+++ bot/models.py
@@ -5,3 +5,8 @@ class User(models.Model):
     id = models.IntegerField(primary_key=True)
     phone = models.CharField(null=True, max_length=40)
     state = models.IntegerField(default=0)
+
+
+class Action(models.Model):
+    type_id = models.IntegerField(primary_key=True)
+    text = models.TextField()
